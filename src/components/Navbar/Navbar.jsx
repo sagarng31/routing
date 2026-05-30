4995777ce29cb715df085f9b81de74bd02a98646
SagarMPatil <sagarng31@gmail.com>
2026-05-30 19:58:06 +0530

    adding basic routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul className='navbar_list'>
-                <li>
-                    <a href='/'>Home</a>
-                </li>
-                <li>
-                    <a href='/products'>Products</a>
-                </li>
-                <li>
-                    <a href='/articles'>Articles</a>
-                </li>
-                <li>
-                    <a href='/admin'>Admin</a>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className="navbar_list">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/articles">Articles</NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin">Admin</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
