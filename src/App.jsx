db27d55624f7bce4f58da406ae2496d7af3c6d2c
SagarMPatil <sagarng31@gmail.com>
2026-05-31 10:25:13 +0530

    created seperate comp for Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Products from "./components/Products/Products";
-import Articles from "./components/Articles/Articles";
-import Admin from "./components/Admin/Admin";
-import NotFound from "./components/NotFound/NotFound";
-import SingleProduct from "./components/Products/SingleProduct";
-import Sales from "./components/Admin/Sales";
-import Sellers from "./components/Admin/Sellers";
-
+import Routing from "./Routing";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
 
       <main className="app_main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<SingleProduct />} />
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/admin" element={<Admin />}>
-            <Route path="/admin/sales" element={<Sales />} />
-            <Route path="/admin/sellers" element={<Sellers />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Routing />
       </main>
     </div>
   );
---
+++ src/Routing.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Articles from "./components/Articles/Articles";
+import Admin from "./components/Admin/Admin";
+import NotFound from "./components/NotFound/NotFound";
+import SingleProduct from "./components/Products/SingleProduct";
+import Sales from "./components/Admin/Sales";
+import Sellers from "./components/Admin/Sellers";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" element={<SingleProduct />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/admin" element={<Admin />}>
+        <Route path="/admin/sales" element={<Sales />} />
+        <Route path="/admin/sellers" element={<Sellers />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default Routing;
