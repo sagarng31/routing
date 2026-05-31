23bed4addf77cc8c51718f9baf4c1fa8ce701e09
SagarMPatil <sagarng31@gmail.com>
2026-05-31 10:15:11 +0530

    query string

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Articles = () => {
-    return (
-        <div>
-            <h2>
-                Articles
-                <p>SortBy: Category:</p>
-            </h2>
-        </div>
-    );
+  const [searchParam, setSearchParam] = useSearchParams();
+  const sortBy = searchParam.get("sortBy");
+  const category = searchParam.get("category");
+
+  const handleSearch = () => {
+    setSearchParam({ sortBy: "city", category });
+  };
+  return (
+    <div>
+      <h2>
+        Articles
+        <p>
+          SortBy: {sortBy} Category: {category}{" "}
+        </p>
+      </h2>
+      <button onClick={handleSearch}>Sort by City</button>
+    </div>
+  );
 };
 
 export default Articles;
